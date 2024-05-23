e7b1db13263776169501f4097dd903669b77008a
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-23 14:48:06 -0400

    add InvaderProject Class

---
+++ js/classes/invaderProjecttile.js
@@ -0,0 +1,8 @@
+class InvaderProjecttile{
+    constructor({position, velocity}){
+        this.position = position;  /* position é uma instancia da classe Invader*/
+        this.velocity = velocity;
+        this.width =3;
+        this.height = 10;
+   }
+}
