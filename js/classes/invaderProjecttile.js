e1baada8ffb3b8ee95cee3f6a6e707d7a8704534
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-23 14:55:09 -0400

    add draw function in InvaderProjecttile class

--- js/classes/invaderProjecttile.js
+++ js/classes/invaderProjecttile.js
@@ -9,4 +9,9 @@ class InvaderProjecttile{
     c.fillstyle = "white";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
    }
+   update(){
+       this.draw();
+       this.position.x += this.velocity.x;
+       this.position.y += this.velocity.y;
+   }
 }
