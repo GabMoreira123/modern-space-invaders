66cf0ff45cf054f48379173e99922b8b54601819
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-23 14:27:02 -0400

    add draw function in Invader class

--- js/classes/invader.js
+++ js/classes/invader.js
@@ -1,24 +1,29 @@
- class invader {
-    constructor({position}){ 
-        this.position = position;
-        this.radius = 10;
-        this.color = " ";
-        this.opacity = 1;
-        this.active = false;
-        gsap.to(this, {
-            radius: 30
-        });
-        const image = new Image();
-        image.src = "./img/invader.png";
-        image.onload = () => {
-            const scale = 1;
-            this.image = image;
-            this.width = image.width *scale;
-            this.height = image.height *scale;
-            this.position ={
-                x: this.position.x,
-                y: this.position.y
-            }
-        }
-    }
- }
+class invader {
+  constructor({ position }) {
+    this.velocity ={
+    x:0,
+    y:0
+    };
+
+    const image = new Image();
+    image.src = "./img/invader.png";
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: this.position.x,
+        y: this.position.y
+      };
+    };
+  }
+  draw(){
+    caches.drawImage(
+        this.image,
+        this.position.x,
+        this.width,
+        this.height
+    );
+  }
+}
