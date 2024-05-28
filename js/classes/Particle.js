fe396726a1d8c494f78efb92e47ab5cdf2208600
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-28 15:53:51 -0400

    add draw function in Particle class

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -4,7 +4,18 @@ class Particle {
         this.velocity = velocity;
         this.radius = radius;
         this.color = color;
-        this.opacity = 1;
+        this.opacity = 1;     // É necessario que haja um parametro
         this.fades = fades;
+    }
+    draw(){
+        c.save();
+        c.globalAlpha = this.opacity;
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI *2);
+        c.closePath();
+        c.fillStyle = this.color; //Define o preenchimento da nossa particula
+        c.fill(); //preenche a cor 
+        c.closePath(); //fecha o caminho
+        c.restore();
     }
 }
