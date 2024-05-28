3753c9f77a99a2840928d420f2089d0974e390ec
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-28 16:37:43 -0400

    add index js

---
+++ index.js
@@ -0,0 +1,6 @@
+const scoreEl = document.querySelector("#scoreEl"); // o hashtag significa id 
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024; //canvas é responsavel por renderizar o jogo
+canvas.height = 576;
